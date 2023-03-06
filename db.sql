5995a8ca6ec21fa25284456ae0bc544f2865a067
Luana <luana00gomes@gmail.com>
2023-03-06 12:56:42 -0300

    Refactor: DB included

--- db.sql
+++ db.sql
@@ -1,12 +1,14 @@
+--CODE WAS RUN ON TERMINAL
 create TABLE foodKind(
     id SERIAL PRIMARY KEY,
     kind VARCHAR(200) NOT NULL
 );
 
+
 create TABLE foodItem(
-    id SERIAL PRIMARY KEY,,
+    id SERIAL PRIMARY KEY,
     img VARCHAR(200), --path to img route
     kind VARCHAR(200),
     descpt VARCHAR(200),
-    price FLOAT(8,2) NOT NULL
+    price FLOAT(24)
 );
